6821f713754dd12100051896ea28abbcefc36ec1
sidtas080504-cloud <sidtas080504@student.jenseneducation.se>
2025-08-26 09:27:03 +0000

    klar

--- uppg2_welcome.py
+++ uppg2_welcome.py
@@ -5,3 +5,5 @@ Välkomna sedan användaren till programmet.
 Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 Hej (Hampus), välkommen till programmet!
 """
+name =input("What is your name")
+print(f"Hey{name}Welcome to my webside")
